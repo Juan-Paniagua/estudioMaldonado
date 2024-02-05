1bb60edf6228b9b941a12d69ecf2f99a98338712
Juan Paniagua <juan.ignacio10396@gmail.com>
2024-02-04 23:46:29 -0300

    V1.07 edits on navbar

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.onscroll = function () {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-130px";
+    document.getElementById("navbar").style.top = "-200px";
   }
   prevScrollpos = currentScrollPos;
 }
